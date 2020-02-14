3a8a526b563cbe072be3b0c3f357bc4a4eca48f5
Kelvin Lee <Kelvin-Lee@users.noreply.github.com>
2020-02-13 17:17:04 -0800

    Add some extra, verbose comments, for sake of learning.

--- app.js
+++ app.js
@@ -1,3 +1,33 @@
+
+/* 
+    `angular` is defined (and usable) here because we loaded the AngularJS CDN before this 
+    app.js file. We loaded the AngularJS CDN in the .html file, in the <head> portion. 
+    In the <head> portion, we can load the CDN before the app.js file.
+
+    To be more clear, we did this...
+
+    ```html
+    <head>
+        <meta charset="utf-8">
+        <title>Angular Boilerplate</title>
+        <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.6/angular.min.js"></script>
+        <script src="app.js"></script>
+    </head>
+    ```html
+
+    ...instead of this...
+
+    ```html
+    <head>
+        <meta charset="utf-8">
+        <title>Angular Boilerplate</title>
+        <script src="app.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.6/angular.min.js"></script>
+    </head>
+    ```html
+
+*/
+
 angular.module('boilerplate', []);
 
 angular.module('boilerplate')
